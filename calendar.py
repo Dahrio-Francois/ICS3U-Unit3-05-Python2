4033c79cb9b8428137243a35223ca59aeea3eaa8
Dahrio-Francois <dahrio.francois@mths.ca>
2022-04-01 18:06:10 +0000

    added calendar.py

---
+++ calendar.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Created by: Dahrio Francois
+# Created on: March 2022
+# This program shows you a month based on a number
+#   with user input
+
+
+def main():
+    # this program makes the calendar
+
+    month = int(input("Enter your number of month(ex: 9 for September: "))
+
+    # input
+    if month == 1:
+        print("\nThis is the month of January")
+
+    elif month == 2:
+        print("\nThis is the month of February")
+
+    elif month == 3:
+        print("\nThis is the month of March")
+
+    elif month == 4:
+        print("\nThis is the month of April")
+
+    elif month == 5:
+        print("\nThis is the month of May")
+
+    elif month == 6:
+        print("\nThis is the month of June")
+
+    elif month == 7:
+        print("\nThis is the month of July")
+
+    elif month == 8:
+        print("\nThis is the month of August.")
+
+    elif month == 9:
+        print("\nThis is the month of September.")
+
+    elif month == 10:
+        print("\nThis is the month of October.")
+
+    elif month == 11:
+        print("\nThis is the month of November.")
+
+    elif month == 12:
+        print("\nThis is the month of December.")
+
+    print("\nDone")
+
+
+if __name__ == "__main__":
+    main()
